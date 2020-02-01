ac9d8dbd0f6bed410ddf2a7cdf80451fe4f612b4
chunhan <44487726+bryan0604@users.noreply.github.com>
2020-02-01 21:14:31 +0800

    2

---
+++ Online Patching Unity Samples/Assets/Resources/Scripts/CloudManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class CloudManager : MonoBehaviour
+{
+    public ImportAssets importAssetScript;
+    [Header("UI")]
+    public GameObject downloadIndicator;
+    public GameObject loadingIndicator;
+    public GameObject image;
+    [SerializeField]
+    private string webPath = null; 
+    ulong downloadedFile;
+    public IEnumerator DownloadImage(System.Action callback)
+    {
+        loadingIndicator.SetActive(true);
+        using (UnityWebRequest www = UnityWebRequest.Get(webPath))
+        {
+            Debug.Log(www.downloadProgress);
+            string path = Path.Combine(Application.dataPath, "testsubject00.jpeg");
+            www.downloadHandler = new DownloadHandlerFile(path);
+       
+            yield return www.SendWebRequest();
+
+            if(www.isNetworkError || www.isHttpError)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadProgress);
+                downloadedFile =  www.downloadedBytes;
+                Debug.Log(downloadedFile);
+
+                if (callback != null)
+                    callback.Invoke();
+
+                StartCoroutine(IndicatorExpiryTimer(2f, loadingIndicator));
+            }
+        }
+    }
+
+    void IndiatorDownload(string _string)
+    {
+        downloadIndicator.SetActive(true);
+
+        Debug.Log("Receiving..." + _string);
+
+        importAssetScript.LoadAssets("testsubject00",image);
+
+        StartCoroutine(IndicatorExpiryTimer(3f, downloadIndicator));
+    }
+
+    IEnumerator IndicatorExpiryTimer(float timer = 2f , GameObject _gameobjectUI = null)
+    {
+        yield return new WaitForSeconds(timer);
+
+        _gameobjectUI.SetActive(false);
+
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartCoroutine(DownloadImage(() => { IndiatorDownload(downloadedFile.ToString()); })) ;
+        }
+    }
+}
---
+++ Online Patching Unity Samples/Assets/Resources/Scripts/ImportAssets.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImportAssets : MonoBehaviour
+{
+    public void LoadAssets(string filename, GameObject image)
+    {
+        Debug.Log("Loading assets...");
+    }
+}
