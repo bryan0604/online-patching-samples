dff8e4baeb7338d0b366c5e67fa0f03ca395d4d5
chunhan <44487726+bryan0604@users.noreply.github.com>
2020-02-19 00:28:06 +0800

    2

---
+++ Online Patching Unity Samples/Assets/Resources/Scripts/Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class Manager : MonoBehaviour
+{
+    public string key;
+    public string name;
+    [SerializeField]
+    bool isloaded;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartCoroutine(Load());
+        }
+        else if(Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            StartCoroutine(PreloadDependencies());
+        }
+    }
+
+    IEnumerator Load()
+    {
+        AsyncOperationHandle asyc = Addressables.LoadAssetAsync<GameObject>(name);
+        yield return asyc.IsDone;
+
+        Debug.Log("ASSET LOADED");
+    }
+
+    IEnumerator PreloadDependencies()
+    {
+        AsyncOperationHandle asyc = Addressables.DownloadDependenciesAsync(key);
+        yield return asyc.IsDone;
+
+        Debug.Log("ASSET DOWNLOADED!");
+    }
+}
