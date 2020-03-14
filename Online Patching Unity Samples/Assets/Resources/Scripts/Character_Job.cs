1fd0bb3c6dca959020c429e186f72a182ad619cb
chunhan <44487726+bryan0604@users.noreply.github.com>
2020-03-14 19:13:57 +0800

    Update online patch

---
+++ Online Patching Unity Samples/Assets/Resources/Scripts/Character_Job.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Job : MonoBehaviour
+{
+    public enum CHARACTER_JOB {boxer, swordsman, archer, wizard, none}
+    public CHARACTER_JOB job = CHARACTER_JOB.none;
+    public List<Color> job_indicator = new List<Color>();
+    public MeshRenderer meshrenderer;
+    void Start()
+    {
+        meshrenderer = GetComponent<MeshRenderer>();
+        meshrenderer.material.color = job_indicator[(int)job];
+    }
+}
