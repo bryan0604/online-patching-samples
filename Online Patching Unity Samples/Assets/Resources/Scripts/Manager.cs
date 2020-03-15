291228518ff215d5e02e51b8fb7a183b984fef92
chunhan <44487726+bryan0604@users.noreply.github.com>
2020-03-16 00:06:40 +0800

    updat e

--- Online Patching Unity Samples/Assets/Resources/Scripts/Manager.cs
+++ Online Patching Unity Samples/Assets/Resources/Scripts/Manager.cs
@@ -2,12 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
 
 public class Manager : MonoBehaviour
 {
     public AssetReference asset;
     private void Start()
     {
-        asset.InstantiateAsync(new Vector3(0,0,0),Quaternion.identity);
+        Addressables.InitializeAsync().Completed += InitialisePatchManager;
     }
+
+    void InitialisePatchManager(AsyncOperationHandle<IResourceLocator> response)
+    {
+        Debug.Log(response);
+        CheckPatch(Addressables.CheckForCatalogUpdates());
+    }
+
+    private void CheckPatch(AsyncOperationHandle<List<string>> callback)
+    {
+        List<string> tempStorage = callback.Result;
+        if(tempStorage!=null)
+        Debug.Log(tempStorage.Count);
+
+        StartCoroutine(Patching());
+    }
+
+    IEnumerator Patching()
+    {
+        AsyncOperationHandle<GameObject> c = asset.InstantiateAsync(new Vector3(0, 0, 0), Quaternion.identity);
+
+        yield return c.IsDone;
+
+        Debug.LogError("Done - Instiantiate");
+    }
+
+    
+    //private IEnumerable<string> Test(string callbackResult)
+    //{
+    //    return Addressables.ca;
+    //}
 }
