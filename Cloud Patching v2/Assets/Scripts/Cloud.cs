52ead8464e6711a5ed5267c527ec68fcbd6368df
ChariotsGamingChunHan <chunhan.chia@chariotsgaming.com>
2020-03-18 15:17:53 +0800

    342

--- Cloud Patching v2/Assets/Scripts/Cloud.cs
+++ Cloud Patching v2/Assets/Scripts/Cloud.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 public class Cloud : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AssetReference image_ar;
+    public GameObject image_go;
+    public Transform parent_canvas;
+
+    void InstantiateAsyncFromCloud()
     {
-        
+        image_ar.InstantiateAsync(new Vector3(), Quaternion.identity, parent_canvas);    
     }
 
-    // Update is called once per frame
-    void Update()
+    void InstantiateFromLocalClient()
     {
-        
+        Instantiate(image_go , parent_canvas);
+    }
+
+    void CheckForUpdates()
+    {
+
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            InstantiateAsyncFromCloud();
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftCommand))
+        {
+            InstantiateFromLocalClient();
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape))
+        {
+
+        }
     }
 }
